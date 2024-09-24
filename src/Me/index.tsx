75b27d03a67db2671e3b666c7171ae25776c7806
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2024-09-24 11:39:49 -0600

    Update index.tsx

--- src/Me/index.tsx
+++ src/Me/index.tsx
@@ -12,21 +12,22 @@ import {
 
 export const RevealBento = () => {
   return (
-    <div className="min-h-screen bg-blue-300 px-4 py-12 text-zinc-50">
+    <div className="min-h-screen px-4 py-12 text-zinc-50">
       <motion.div
         initial="initial"
         animate="animate"
         transition={{
           staggerChildren: 0.05,
         }}
-        className="mx-auto grid max-w-4xl grid-flow-dense grid-cols-12 gap-4"
+        className="mx-auto grid max-w-6xl grid-flow-dense grid-cols-12 gap-4"
       >
         <HeaderBlock />
         <SocialsBlock />
         <AboutBlock />
-        <SocialsBlock />
-        <SocialsBlock />
         <EmailListBlock />
+        <LocationBlock />
+        <WorkBlock />
+        <SocialsBlock />
       </motion.div>
       <Footer />
     </div>
@@ -177,74 +178,64 @@ const WorkBlock = () => (
       rotate: "2.5deg",
       scale: 1.1,
     }}
-    className="col-span-6 bg-blue-500 md:col-span-3"
-  ></Block>
+    className="col-span-6 bg-slate-700 md:col-span-3"
+  >
+    <h1>Prueba tecnica</h1>
+    <p>Buscador de Peliculas con uso de JSON </p>
+  </Block>
 );
 
 const AboutBlock = () => (
-  <Block className="col-span-12 text-3xl leading-snug">
+  <Block className="col-span-12 text-xl leading-snug">
+    <strong>🚀 Una apasionada por el desarrollo y la programación 🧪</strong>
     <p>
-      <strong>🚀 Una apasionada por el desarrollo y la programación 🧪</strong>
-      Soy un desarrollador FrontEnd JavaScript con dos años de experiencia como
-      desarrollador Frontend. Me apasiona mucho el desarrollo web y en general
-      la creación de productos digitales. Me gusta el camino de aprendizaje
-      continuo, los retos y enfocada en identificar áreas de mejora después cada
-      proyecto, con el objetivo de entregar un producto de alta calidad.
+      Soy una desarrolladora FrontEnd JavaScript con dos años de experiencia. Me
+      apasiona mucho el desarrollo web y en general la creación de productos
+      digitales. Me gusta el camino de aprendizaje continuo, los retos y
+      enfocada en identificar áreas de mejora después cada proyecto, con el
+      objetivo de entregar un producto de alta calidad.
     </p>
   </Block>
 );
 
 const LocationBlock = () => (
-  <Block className="col-span-12 flex flex-col items-center gap-4 md:col-span-3">
-    <FiMapPin className="text-3xl" />
-    <p className="text-center text-lg text-zinc-400">Cyberspace</p>
+  <Block className="col-span-12 flex flex-col justify-center items-center gap-4 md:col-span-3">
+    <a
+      className="text-center text-lg text-zinc-400"
+      href="https://maps.app.goo.gl/h3c4yKZg217Vzhf38"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FiMapPin className="text-center text-3xl" />
+      Morelia Michoacan
+    </a>
   </Block>
 );
 
 const EmailListBlock = () => (
   <Block className="col-span-12 md:col-span-9">
-    <p className="mb-3 text-lg">Join my mailing list</p>
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      className="flex items-center gap-2"
-    >
-      <input
-        type="email"
-        placeholder="Enter your email"
-        className="w-full rounded border border-zinc-700 bg-zinc-800 px-3 py-1.5 transition-colors focus:border-red-300 focus:outline-0"
-      />
-      <button
-        type="submit"
-        className="flex items-center gap-2 whitespace-nowrap rounded bg-zinc-50 px-3 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-300"
-      >
-        <FiMail /> Join the list
-      </button>
-    </form>
+    <p className="mb-3 text-lg">
+      Aquí hay algunas tecnologías con las que he trabajado:
+      <br />
+      <div className="grid grid-cols-2">
+        <p>
+          Tecnologias Usuales <br />
+          React.js <br />
+          Next.js <br />
+          JavaScript (ES6+) <br />
+          TailwindCSS <br />
+          TypeScript
+        </p>
+        <p>
+          Tecnologías no usuales <br />
+          Node.js <br />
+          Express.js <br />
+          Figma <br />
+        </p>
+      </div>
+    </p>
   </Block>
 );
-
-const Logo = () => {
-  // Temp logo from https://logoipsum.com/
-  return (
-    <svg
-      width="40"
-      height="auto"
-      viewBox="0 0 50 39"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="mx-auto mb-12 fill-zinc-50"
-    >
-      <path
-        d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
-        stopColor="#000000"
-      ></path>
-      <path
-        d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
-        stopColor="#000000"
-      ></path>
-    </svg>
-  );
-};
 
 const Footer = () => {
   return (
