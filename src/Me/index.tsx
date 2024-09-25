717fbe0f5db7297bb58cfacceba9643e153f18e4
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2024-09-25 17:40:52 -0600

    Update index.tsx

--- src/Me/index.tsx
+++ src/Me/index.tsx
@@ -21,8 +21,6 @@ export const RevealBento = () => {
         <SocialsBlock />
         <AboutBlock />
         <ListBlock />
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        {/* <LocationBlock /> */}
         <WorkBlock />
       </motion.div>
       <Footer />
@@ -325,22 +323,6 @@ const AboutBlock = () => (
     </p>
   </Block>
 );
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const LocationBlock = () => (
-//   <Block className="col-span-12 flex flex-col justify-center items-center gap-4 md:col-span-3">
-//     <a
-//       className="text-center text-lg text-zinc-400"
-//       href="https://maps.app.goo.gl/h3c4yKZg217Vzhf38"
-//       target="_blank"
-//       rel="noopener noreferrer"
-//     >
-//       <FiMapPin className="text-center text-3xl" />
-//       Morelia Michoacan
-//     </a>
-//   </Block>
-// );
-
 const ListBlock = () => (
   <Block className="col-span-12 md:col-span-6">
     <div className="mb-3 text-lg">
