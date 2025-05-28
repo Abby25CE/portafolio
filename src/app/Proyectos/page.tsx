c86bc9fa3540e1389fc71f403464d2fcd8f00455
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2025-05-27 22:33:17 -0600

    Update

--- src/app/Proyectos/page.tsx
+++ src/app/Proyectos/page.tsx
@@ -4,7 +4,7 @@ import HoverDevCards from "@/Proyects/Index";
 
 export default function Home() {
   return (
-    <div className="bg-blue-300 w-full min-h-screen pb-12">
+    <div className="bg-teal-700 w-full min-h-screen pb-12">
       <HoverDevCards></HoverDevCards>
     </div>
   );
