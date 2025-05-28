7a9dda967752058757adce13b4a3bf308576bd82
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2025-05-27 22:28:07 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/Hero/index";
 import { RevealBento } from "@/Me/index";
 export default function Home() {
   return (
-    <div className="bg-blue-300">
+    <div className="bg-slate-600">
       <Hero />
       <RevealBento />
     </div>
